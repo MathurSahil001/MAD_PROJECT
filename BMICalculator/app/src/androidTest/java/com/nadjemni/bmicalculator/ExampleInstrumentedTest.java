6a2e7b67611715f7f1db6e8c21d31ec88c5e4824
Sahitya Tripathi <108547977+SahityaTr1@users.noreply.github.com>
2023-05-19 23:17:30 +0530

    ExampleInstrumentedTest.java

--- BMICalculator/app/src/androidTest/java/com/nadjemni/bmicalculator/ExampleInstrumentedTest.java
+++ BMICalculator/app/src/androidTest/java/com/nadjemni/bmicalculator/ExampleInstrumentedTest.java
@@ -8,16 +8,10 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         androidx.test.platform.app.ApplicationProvider.getApplicationContext();
         String appContext = InstrumentationRegistry.getInstrumentation().getTargetContext().getPackageName();
         assertEquals("com.nadjemni.bmicalculator", appContext);
